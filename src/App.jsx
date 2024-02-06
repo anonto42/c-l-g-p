bdec0cfc6986ea3c185d14b2010f67fe5226a11a
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-02-06 17:50:03 +0600

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './Pages/Home';
+import Naveber from './Components/Naveber/Naveber';
+import Footer from './Components/Footer/Footer';
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+    <Naveber/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   )
 }
 
--- src/Components/Naveber/Naveber.jsx
+++ src/Components/Naveber/Naveber.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 const Naveber = () => {
+  
   return (
-    <div>Naveber</div>
+    <div>
+    </div>
   )
 }
 
